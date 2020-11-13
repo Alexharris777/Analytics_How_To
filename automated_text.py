4e94ff6d2829a1293e9cb725914d2be57566e942
Alexharris777 <60671600+Alexharris777@users.noreply.github.com>
2020-11-12 20:39:52 -0600

    Create automated_text.py

---
+++ automated_text.py
@@ -0,0 +1,6 @@
+#Automated Text
+paragraph = f'''Flu Paragraph/
+Overall Flu Related acute admits activity decreased to minimal from low month over month, ({monthname} {year} Flu Related ADK = {fluadk}). The Non Flu Season Flu Related ADK average is 38.1. In {monthname} {year}, the {flumonthname}th month of the {year} Flu Season, Flu Related ADK for the Medicare Segment was {fluadk}. This is a {yoychange}% change in Flu Related ADK compared to last year {monthname} {previousyear}, where Flu Related ADK was {previousfluadk}. The {reg1} and {reg2} Regulatory Markets have the highest {year} Flu Season - Flu Related ADK with values of {flu1} and {flu2} respectively. The {mreg1} and {mreg2} Regulatory Markets have the highest MOM change in Flu Related ADK with values of {mflu1}% and {mflu2}% respectively. The {reg3} and {reg4} Regulatory Markets have the lowest Flu Related ADK with values of {flu3} and {flu4} respectively. The {mreg3} and {mreg4} Regulatory Markets have the highest MOM change with the values of {mflu3}% and {mflu4}%.
+'''
+
+print(paragraph)
